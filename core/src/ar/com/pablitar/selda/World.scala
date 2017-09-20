2fad03beee231f85bb815cce1cbac4cf881b00cc
pablitar <pablitar@gmail.com>
2017-09-20 01:13:57 -0300

    Agregado shaker para cuando hace un ataque también

--- core/src/ar/com/pablitar/selda/World.scala
+++ core/src/ar/com/pablitar/selda/World.scala
@@ -16,16 +16,21 @@ import ar.com.pablitar.selda.effects.ImpactEffect
 import ar.com.pablitar.selda.npc.NPC
 
 class World(val map: TiledMap) {
-  
+
   val timeDelay = new TimeDelay(0.8f, 0.6f, stopTimeDuration = 0.07f)
 
   val player = new Player(playerStartingPosition, this)
   val elements = new DelayedRemovalBuffer[SeldaElement](player)
   createNPCS()
 
-  val cameraShaker = new Shaker(new RandomVectorInRange(3f, 6f), new RandomFloatInRange(0.3f, 0.4f), new RandomVectorInRange(6, 10))
-  
-  player.onAttackReceived = () => cameraShaker.generateShake()
+  val attackReceivedShaker = new Shaker(new RandomVectorInRange(3f, 6f), new RandomFloatInRange(0.4f, 0.6f), new RandomVectorInRange(6, 10))
+  val attackGivenShaker = new Shaker(new RandomVectorInRange(2f, 2f), new RandomFloatInRange(0.1f, 0.2f), new RandomVectorInRange(10, 15))
+  var cameraShaker = attackReceivedShaker
+
+  player.onAttackReceived = { () =>
+    attackReceivedShaker.generateShake()
+    cameraShaker = attackReceivedShaker
+  }
 
   def renderables = elements.elements
 
@@ -37,7 +42,7 @@ class World(val map: TiledMap) {
   def update(delta: Float) = {
     timeDelay.update(delta)
     val actualDelta = timeDelay.apply(delta)
-    cameraShaker.update(actualDelta)
+    cameraShaker.update(delta)
     for (element <- elements) element.update(delta, actualDelta)
     elements.commitRemoval()
   }
@@ -51,9 +56,11 @@ class World(val map: TiledMap) {
 
   def createNPC(p: Vector2) = {
     val npc = new NPC(p, this)
-    npc.onAttackReceived = {() =>
+    npc.onAttackReceived = { () =>
       timeDelay.startDelaying
       addImpactEffect(npc.position)
+      attackGivenShaker.generateShake()
+      cameraShaker = attackGivenShaker
     }
     npc
   }
@@ -67,7 +74,7 @@ class World(val map: TiledMap) {
   }
 
   def npcs = {
-    elements.elements.collect {case npc: NPC => npc}
+    elements.elements.collect { case npc: NPC => npc }
   }
 
 }
