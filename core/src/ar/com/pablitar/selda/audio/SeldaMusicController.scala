371a8f377ea6b053418c5915241d2e81f16028b9
pablitar <pablitar@gmail.com>
2017-10-09 00:46:37 -0300

    Bajado el volumen de la música

--- core/src/ar/com/pablitar/selda/audio/SeldaMusicController.scala
+++ core/src/ar/com/pablitar/selda/audio/SeldaMusicController.scala
@@ -5,13 +5,14 @@ import ar.com.pablitar.selda.RestOfTheWorld
 import ar.com.pablitar.selda.WorldArea
 
 object SeldaMusicController extends MusicController {
+  musicVolume = 0.7f
   var currentMusicArea: WorldArea = RestOfTheWorld
 
   def playMusicForArea(currentPlayerArea: WorldArea) = {
     if(currentMusicArea != currentPlayerArea) {
       println("Playing music for area: " + currentPlayerArea)
       currentMusicArea = currentPlayerArea
-      fadeOutAndPlay(currentPlayerArea.trackGetter, 0.5f)
+      fadeOutAndPlay(currentPlayerArea.trackGetter, 1.5f)
     }
   }
 }
